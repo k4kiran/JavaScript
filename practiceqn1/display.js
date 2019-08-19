d6e57e4958ddce5a03e793b3e2dbbda1b8d36cd4
T0588 <kiran.bal@triassicsolutions.com>
2019-08-19 11:46:26 +0530

    completed basic form

--- practiceqn1/display.js
+++ practiceqn1/display.js
@@ -1,23 +1,24 @@
+function getGenderDetails()
+{
+  var newgender=document.getElementsByName('optgender');
+  for(i=0;i<newgender.length;i++)
+  {
+    if(newgender[i].checked)
+    {
+      return newgender[i].value;
+    }
+  }
+}
+
 function displayDetails(){
- 
-
   document.getElementById('detailsemail').innerHTML = document.getElementById("inputemail").value;
-
-  document.getElementById('detailsname').innerHTML = document.getElementById("inputname1").value + document.getElementById("inputname2").value;
-
-  document.getElementById('detailsradio').innerHTML = document.getElementById("radio1").value;
-
-  document.getElementById('detailsphone1').innerHTML = document.getElementById("inputphone1").value;
-
-  document.getElementById('detailsphone2').innerHTML = document.getElementById("inputphone2").value;
-
+  document.getElementById('detailsname').innerHTML = document.getElementById("inputname1").value + " " + document.getElementById("inputname2").value;
+  var gendervalue = getGenderDetails();
+  document.getElementById('detailsradio').innerHTML = gendervalue;
+  document.getElementById('detailsphone2').innerHTML = document.getElementById("inputphone1").value + " " +document.getElementById("inputphone2").value;
   document.getElementById('detailsstreet').innerHTML = document.getElementById("inputstreet").value;
-
   document.getElementById('detailscity').innerHTML = document.getElementById("inputcity").value;
-
   document.getElementById('detailsstate').innerHTML = document.getElementById("inputstate").value;
-
   document.getElementById('detailscode').innerHTML = document.getElementById("inputcode").value;
-  
   document.getElementById("showdetails").style.display = "block";
 }
--- practiceqn1/validation.js
+++ practiceqn1/validation.js
@@ -1,9 +1,27 @@
 function validateForm()
 {
-  //validating name
-  var uname = document.getElementById("inputname1").value;
-  console.log("printing" + uname );
-  
+  //validating first name
+  var namefirst = document.getElementById("inputname1").value;
+  console.log("my first name is " + namefirst);
+  var textformat = /^[A-Za-z]+$/ 
+  if(namefirst.match(textformat)){
+    console.log("first name matched");
+  }
+  else{
+    alert("first name not matched");
+  }
+
+  //validating last name
+  var namelast = document.getElementById("inputname2").value;
+  console.log("my last name is " + namefirst);
+  var tetformat = /^[A-Za-z]+$/ 
+  if(namefirst.match(textformat)){
+    console.log("last name matched");
+  }
+  else{
+    alert("last name not matched");
+  }
+
   //validating phone number
   var phnumber = document.getElementById("inputphone2").value;
   console.log("my number is " + phnumber);
@@ -15,6 +33,28 @@ function validateForm()
     alert("number not matched");
   }
 
+  //validating city
+  var city = document.getElementById("inputcity").value;
+  console.log("my last name is " + city);
+  var textformat = /^[A-Za-z]+$/ 
+  if(city.match(textformat)){
+    console.log("city  matched");
+  }
+  else{
+    alert("city not matched");
+  }
+
+  //validating state
+  var state = document.getElementById("inputstate").value;
+  console.log("my state is " + state);
+  var textformat = /^[A-Za-z]+$/ 
+  if(state.match(textformat)){
+    console.log("state  matched");
+  }
+  else{
+    alert("state not matched");
+  }
+
   //validating zip code
   var zipcode = document.getElementById("inputcode").value;
   console.log("my zipcode is " + zipcode);
@@ -24,26 +64,5 @@ function validateForm()
   }
   else{
     alert(" zipcode not matched");
-  }
-  //validating first name
-  var namefirst = document.getElementById("inputname1").value;
-  console.log("my first name is " + namefirst);
-  var fnameformat = /^[A-Za-z]+$/ 
-  if(namefirst.match(fnameformat)){
-    console.log("first name matched");
-  }
-  else{
-    alert("first name not matched");
-  }
-
-  //validating last name
-  var namelast = document.getElementById("inputname2").value;
-  console.log("my last name is " + namefirst);
-  var fnameformat = /^[A-Za-z]+$/ 
-  if(namefirst.match(fnameformat)){
-    console.log("last name matched");
-  }
-  else{
-    alert("last name not matched");
   }
 }
