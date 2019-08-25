4248435e5b1f4ae49eea1c410eed4860ddf1e0df
k4kiran <kforkiran005@gmail.com>
2019-08-25 20:07:26 +0530

    new tips added

--- extras/cssqn2modified/jsonbootstrap.js
+++ extras/cssqn2modified/jsonbootstrap.js
@@ -22,21 +22,56 @@ function createCategory() {
       },
       {
         "categoryTitle": "Multimedia",
-        "Count": "500",
+        "Count": "400",
+        "description": "Open Position",
+      },
+      {
+        "categoryTitle": "Multimedia",
+        "Count": "300",
         "description": "Open Position",
       }
   ]
 
   $(document).ready(function(){
-    
+    var flag=true;
     var catListItemTitle = " ";
-    for(var catItem = 0; catItem < myCategory.length; catItem++){
-        
-        catListItemTitle = catListItemTitle + '<li><div="class="category" text-dark"><label class="categoryhead">'+ myCategory[catItem].categoryTitle + '</label>';
-        catListItemTitle = catListItemTitle + '<label class="opennumber text-danger">' + myCategory[catItem].Count +'</label>&nbsp;';
-        catListItemTitle = catListItemTitle + '<label class="poststatus text-secondary">' + myCategory[catItem].description + '</label><hr></div></li>';         
-        $(".testlist").html(catListItemTitle);
+    var input1 = document.getElementById("dropdown1").value;
+    var input2 = document.getElementById("dropdown2").value;
+    var input3 = document.getElementById("dropdown3").value;
+    for(var catItem = 0; catItem < myCategory.length; catItem++)
+    {
+      if(input1.match(myCategory[catItem].categoryTitle))
+      {
+        if(input2.match(myCategory[catItem].Count))
+        {
+          if(input3.match(myCategory[catItem].description))
+          {
+            catListItemTitle = catListItemTitle + '<li><div class="category" text-dark"><label class="categoryhead">'+ myCategory[catItem].categoryTitle + '</label>';
+            catListItemTitle = catListItemTitle + '<label class="opennumber text-danger">' + myCategory[catItem].Count +'</label>&nbsp;';
+            catListItemTitle = catListItemTitle + '<label class="poststatus text-secondary">' + myCategory[catItem].description + '</label><hr></div></li>';         
+            
+          } 
+        }      
+      }
     }
+    if(!catListItemTitle.match(""))
+    {
+      $(".testlist").html("<li><p>No Data Found</p></li>");
+    }
+    else
+    {
+      $(".testlist").html(catListItemTitle);
+    }
+    
 
 });
+}
+
+function getInput(){
+  var input1 = document.getElementById("dropdown1").value;
+  console.log(input1);
+  var input2 = document.getElementById("dropdown2").value;
+  console.log(input2);
+  var input3 = document.getElementById("dropdown3").value;
+  console.log(input3);
 }
